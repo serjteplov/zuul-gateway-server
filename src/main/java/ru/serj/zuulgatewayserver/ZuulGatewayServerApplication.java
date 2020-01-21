6a0728097919bed7af575c0fa08d44b36a785a08
serg <serj.teplov@gmail.com>
2020-01-21 08:30:00 +0300

    yet another commit

---
+++ src/main/java/ru/serj/zuulgatewayserver/ZuulGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package ru.serj.zuulgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/serj/zuulgatewayserver/ZuulGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serj.zuulgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
