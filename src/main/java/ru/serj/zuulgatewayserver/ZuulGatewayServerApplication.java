962e2fcfe59316c70b661c21992c1712af46fb1f
serg <serj.teplov@gmail.com>
2020-01-28 08:27:06 +0300

    zuul

--- src/main/java/ru/serj/zuulgatewayserver/ZuulGatewayServerApplication.java
+++ src/main/java/ru/serj/zuulgatewayserver/ZuulGatewayServerApplication.java
@@ -2,7 +2,11 @@ package ru.serj.zuulgatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulGatewayServerApplication {
 
---
+++ src/main/java/ru/serj/zuulgatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,35 @@
+package ru.serj.zuulgatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info("request -> {} request uri -> {}", request, request.getRequestURI());
+        return null;
+    }
+}
