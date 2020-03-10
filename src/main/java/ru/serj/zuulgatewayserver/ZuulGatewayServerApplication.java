fa86793ee1bf5d395f0705ce044978d5fe6ff907
serg <serj.teplov@gmail.com>
2020-03-10 15:55:32 +0300

    zuul

--- src/main/java/ru/serj/zuulgatewayserver/ZuulGatewayServerApplication.java
+++ src/main/java/ru/serj/zuulgatewayserver/ZuulGatewayServerApplication.java
@@ -1,9 +1,11 @@
 package ru.serj.zuulgatewayserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,6 +14,11 @@ public class ZuulGatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
